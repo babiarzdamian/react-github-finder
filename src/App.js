ee7ad04863e8e3aeb9781d9186c11ed34892ff4b
Damian Babiarz <babiarz_damian@wp.pl>
2021-07-16 00:04:51 +0200

    feat: add navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hi!</h1>
+                <Navbar title="React Github Finder" />
             </div>
         );
     }
