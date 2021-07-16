dcb2af4a1a999b77eba747a8f1f08b658f681294
Damian Babiarz <babiarz_damian@wp.pl>
2021-07-17 00:09:46 +0200

    feat: add users component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-    };
-
-    render() {
-        const { avatar_url, login, html_url } = this.state;
-
-        return (
-            <div className="card text-center">
-                <img
-                    src={avatar_url}
-                    alt=""
-                    className="round-img"
-                    style={{
-                        width: '60px',
-                        border: 'dotted 3px var(--primary-color)',
-                    }}
-                />
-                <h3>{login}</h3>
-                <div>
-                    <a
-                        href={html_url}
-                        className="btn btn-light btn-sm my-1"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        MORE
-                    </a>
-                </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{
+                    width: '60px',
+                    border: 'dotted 3px var(--primary-color)',
+                }}
+            />
+            <h3>{login}</h3>
+            <div>
+                <a
+                    href={html_url}
+                    className="btn btn-light btn-sm my-1"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    MORE
+                </a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
